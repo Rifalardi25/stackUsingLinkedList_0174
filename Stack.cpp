8e86aa29c107fa42a2614fb82b77536ba5790e70
RifalArdi25 <rifalardi25@gmail.com>
2025-05-22 11:02:04 +0700

    Membuat prosedur peek/top operation

--- Stack.cpp
+++ Stack.cpp
@@ -50,6 +50,24 @@ class Stack
         cout << "Popped value: " << top->data<< endl;
     }
 
+    // Peek/Top operation: Retrieve the value of the topmost element without removing
+    void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is empty." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " << endl;
+                current = current->next;
+            }
+            cout << endl;
+        } // Return the value of the top node
+    }
 
 };
 
