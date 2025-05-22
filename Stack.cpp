c940f79555db8816e5d0e7430d3b76c8e0ff7e70
RifalArdi25 <rifalardi25@gmail.com>
2025-05-22 11:14:39 +0700

    Menampilkan pilihan menu

--- Stack.cpp
+++ Stack.cpp
@@ -84,7 +84,14 @@ int main()
 
     while (choice != 4)
     {
+        cout << "1. Push\n";
+        cout << "2. Pop\n";
+        cout << "3. Peek\n";
+        cout << "4. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
 
+        
     }
     return 0;
 }
